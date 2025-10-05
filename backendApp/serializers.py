92c377ad096ad94346adba6fdebc5f8dcb781e90
tabsheermk <creativemk12@gmail.com>
2025-10-05 13:18:02 +0530

    serializers for other models created

--- backendApp/serializers.py
+++ backendApp/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from backendApp.models import Movie, Show, Booking
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -15,4 +16,22 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'duration_minutes']
+
+class ShowSerializer(serializers.ModelSerializer):
+    movie = MovieSerializer(read_only=True)
+    class Meta:
+        model = Show
+        fields = ['id', 'movie', 'screen_name', 'date_time', 'total_seats']
+
+class BookingSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    show = ShowSerializer(read_only=True)
+    class Meta: 
+        model = Booking
+        fields = ['id', 'user', 'show', 'seat_number', 'status', 'created_at']
 
